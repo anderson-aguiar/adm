edfcc2b4925e18ca478d2c427f997c7b9efcc467
Anderson Martins de Aguiar <anderson.aguiar@agi.com.br>
2026-02-02 09:57:14 -0300

    Configs iniciais

---
+++ src/test/java/br/com/agi/adm/AdmApplicationTests.java
@@ -0,0 +1,13 @@
+package br.com.agi.adm;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class AdmApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
